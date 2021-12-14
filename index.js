eae15b30450fffeca3b3a1c6ab0c258c3ed89d36
VikashTiwarii296 <83680360+VikashTiwarii296@users.noreply.github.com>
2021-12-14 11:41:19 +0530

    update index.js file

--- index.js
+++ index.js
@@ -1,105 +1,47 @@
-let form=document.querySelector("form");
-let text=document.getElementById("text");
-let todoCon=document.querySelector(".todo-con")
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    addtodo();
-})
-let todos=JSON.parse(localStorage.getItem("todos"));
-if(todos){
-    todos.forEach(element => {
-        addtodo(element)
+let submit=document.querySelector(".submit");
+let notesElem=document.querySelector('.notes');
+let title=document.querySelector("#text");
+let desc=document.querySelector('#desc');
+let notes=JSON.parse(localStorage.getItem("notes"));
+if(notes){
+    notes.forEach(element => {
+        addNotes(element)
     });
 }
-function addtodo(elem) {
-    let todoColl=document.createElement("div");
-    todoColl.classList.add("todocoll")
-    let todotext=text.value;
-    if(elem){
-        todotext=elem.text;
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    addNotes()
+})
+function addNotes(obj) {
+    let card=document.createElement("div");
+    card.classList.add("card");
+    let titleval=title.value;
+    let descVal=desc.value;
+    if(obj){
+        titleval=obj.title;
+        descVal=obj.desc;
     }
-    if(todotext){
-    todoColl.innerHTML=`
-    <div class="todo-li">
-    <div class="check ${elem && elem.complete?"active-check":""}"><img src="./images/icon-check.svg" alt=""></div>
-    <p class="ptag ${elem && elem.complete?"complete":""}">${todotext}</p>
-    <button class="close"><img src="./images/icon-cross.svg" alt=""></button>
-  </div>
-  <div class="hr"></div>`;
-    todoCon.appendChild(todoColl);
-    updateLs()
+    if(titleval){
+        card.innerHTML=`<h3>${titleval}</h3>
+                                    <p class="ptag">${descVal}</p>
+                             <button class="del">Delete</button>`;
+        notesElem.appendChild(card);
+        updateLs()
     }
-    let close=todoColl.querySelector(".close");
-    close.addEventListener("click", ()=>{
-        todoColl.remove();
+    let del=card.querySelector(".del");
+    del.addEventListener('click', ()=>{
+        card.remove();
         updateLs();
     })
-    let check=todoColl.querySelector(".check");
-    check.addEventListener('click', ()=>{
-        check.classList.toggle("active-check")
-        todoColl.children[0].children[1].classList.add("complete")  
-        updateLs()
-    })
-    text.value="";
 }
-
 function updateLs() {
-    let ptag=document.querySelectorAll(".ptag")
+    let card=document.querySelectorAll(".card");
     let arr=[];
-    ptag.forEach(element => {
+    card.forEach(element => {
         arr.push({
-            text:element.innerText,
-            complete:element.classList.contains("complete")
+            title:element.children[0].innerText,
+            desc:element.children[1].innerText
         })
     });
-    localStorage.setItem("todos",JSON.stringify(arr));
+    localStorage.setItem("notes", JSON.stringify(arr));
 }
-let info=document.querySelectorAll(".choice p")
-let todoli=document.querySelectorAll(".todocoll")
-console.log(info);
-info.forEach(element => {
-   element.addEventListener("click", ()=>{
-    info.forEach(item => {
-        item.classList.remove("active");
-    });
-    element.classList.add("active")
-    if(element.innerText=="Active"){
-        todoli.forEach(elem => {
-            if(!elem.children[0].children[1].classList.contains("complete")){
-                elem.style.display="block";
-            }else{
-                elem.style.display="none";
-            }
-        });
-    }else if(element.innerText=="Completed"){
-        todoli.forEach(elem => {
-            if(elem.children[0].children[1].classList.contains("complete")){
-                elem.style.display="block";
-            }else{
-                elem.style.display="none";
-            }
-        });
-    }else{
-        todoli.forEach(elem => {
-            elem.style.display="block";
-        });
-    }
-   })
-});
-let clear=document.querySelector(".clear");
-clear.addEventListener("click", ()=>{
-    todoli.forEach(elem => {
-        if(elem.children[0].children[1].classList.contains("complete")){
-            elem.remove()
-            updateLs();
-        }
-    });
-})
-let left=document.querySelector(".left");
-function setitem() {
-    let activeTodo=document.querySelectorAll(".todo-li .active-check");
-    let diff=todoli.length-activeTodo.length;
-    left.innerText=`${diff} items left`
-    
-}
-setitem();
